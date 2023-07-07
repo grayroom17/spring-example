dc6f016b85c0f69c1ccaa079bad4eb064f6d8093
DeevS <deev@advantum.pro>
2023-07-07 20:43:45 +0300

    Application.xml and firs bean. Bean id and bean names

--- src/main/java/com/example/spring/ApplicationRunner.java
+++ src/main/java/com/example/spring/ApplicationRunner.java
@@ -1,22 +1,15 @@
 package com.example.spring;
 
-import com.example.spring.database.repository.CompanyRepository;
-import com.example.spring.database.repository.UserRepository;
 import com.example.spring.database.utils.ConnectionPool;
-import com.example.spring.ioc.Container;
-import com.example.spring.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Slf4j
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-
-//        ConnectionPool connectionPool = new ConnectionPool();
-//        UserRepository userRepository = new UserRepository(connectionPool);
-//        CompanyRepository companyRepository = new CompanyRepository(connectionPool);
-//        UserService userService = new UserService(userRepository, companyRepository);
-
-        Container container = new Container();
-        UserService userService = container.get(UserService.class);
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        log.info(">>>>>>>>> " + context.getBean("pool1", ConnectionPool.class) + " <<<<<<<<<<<");
     }
 
 }
