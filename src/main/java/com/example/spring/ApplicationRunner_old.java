cf3663a602829224608b9f13a1b3bce3e5fd5c2c
DeevS <mygrayroom@gmail.com>
2023-07-18 21:27:20 +0300

    Migrate to SpringBoot app

---
+++ src/main/java/com/example/spring/ApplicationRunner_old.java
@@ -0,0 +1,36 @@
+//package com.example.spring;
+//
+//import com.example.spring.config.ApplicationConfiguration;
+//import com.example.spring.database.repository.CompanyRepository;
+//import com.example.spring.database.utils.ConnectionPool;
+//import com.example.spring.service.DepartmentService;
+//import com.example.spring.service.UserService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//
+//@Slf4j
+//public class ApplicationRunner {
+//
+//    private static String formattedString = ">>>>>>>>> %s <<<<<<<<<<<";
+//
+//    public static void main(String[] args) {
+//        CompanyRepository companyRepository;
+////        переход на AnnotationConfigApplicationContext
+////        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml")) {
+//        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
+//
+//            ConnectionPool singletonConnectionPool1 = context.getBean("pool2", ConnectionPool.class);
+//            log.info(formattedString.formatted(singletonConnectionPool1));
+//
+//            companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+//            log.info(formattedString.formatted(companyRepository));
+//
+//            UserService userService = context.getBean("userService", UserService.class);
+//            log.info(formattedString.formatted(userService));
+//
+//            DepartmentService departmentService = context.getBean("departmentService", DepartmentService.class);
+//            log.info(departmentService.findById(1).toString());
+//        }
+//    }
+//
+//}
--- src/main/java/com/example/spring/config/JpaConfiguration.java
+++ src/main/java/com/example/spring/config/JpaConfiguration.java
@@ -1,11 +1,11 @@
 package com.example.spring.config;
 
 import com.example.spring.condition.JpaCondition;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 
 @Slf4j
 @Conditional(JpaCondition.class)
