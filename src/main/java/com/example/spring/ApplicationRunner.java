33b1f0ddb8d2ff1ba11aab62d109ff9f368bbeed
DeevS <mygrayroom@gmail.com>
2023-07-19 20:56:34 +0300

    Lombok plugin

--- src/main/java/com/example/spring/ApplicationRunner.java
+++ src/main/java/com/example/spring/ApplicationRunner.java
@@ -1,13 +1,17 @@
 package com.example.spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRunner.class,args);
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class, args);
+        log.info(String.valueOf(context.getBeanDefinitionCount()));
     }
 
 }
--- src/main/java/com/example/spring/database/utils/ConnectionPool.java
+++ src/main/java/com/example/spring/database/utils/ConnectionPool.java
@@ -3,6 +3,7 @@ package com.example.spring.database.utils;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,20 +12,18 @@ import org.springframework.stereotype.Component;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
+@RequiredArgsConstructor
 @Component("pool1")
 public class ConnectionPool {
 
-    private final String username;
-    private final Integer poolSize;
+    @Value("${db.username}")
+    String username;
+    @Value("${db.pool.size}")
+    Integer poolSize;
     /*List<Object> args;
     @NonFinal
     @Setter
     private Map<String, Object> properties;*/
-
-    public ConnectionPool(@Value("${db.username}") String username, @Value("${db.pool.size}") Integer poolSize) {
-        this.username = username;
-        this.poolSize = poolSize;
-    }
 
     @PostConstruct
     private void init() {
