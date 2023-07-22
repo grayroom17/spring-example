fa952da1db27b0c1bd53262c9bb76f591f8756ee
DeevS <mygrayroom@gmail.com>
2023-07-22 15:10:45 +0300

    Spring integration test part 2

--- src/test/java/com/example/spring/it/service/DepartmentServiceIT.java
+++ src/test/java/com/example/spring/it/service/DepartmentServiceIT.java
@@ -1,28 +1,32 @@
 package com.example.spring.it.service;
 
+import com.example.spring.config.DatabasePropertiesWithConfigurationProperties;
 import com.example.spring.dto.DepartmentReadDto;
 import com.example.spring.service.DepartmentService;
 import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@FieldDefaults(level = AccessLevel.PRIVATE)
-//@ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+/*@TestConstructor(autowireMode = ALL)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = ApplicationRunner.class, initializers = ConfigDataApplicationContextInitializer.class)*/
 @SpringBootTest
+@ActiveProfiles("test")
 class DepartmentServiceIT {
+    static Integer COMPANY_ID = 1;
 
-    static final Integer COMPANY_ID = 1;
-
-    @Autowired
     DepartmentService departmentService;
+    DatabasePropertiesWithConfigurationProperties databaseProperties;
 
     @Test
     void findById() {
