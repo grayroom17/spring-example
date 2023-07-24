3995cc09b2012a02006115e70a0880477fcb1d16
DeevS <deev@advantum.pro>
2023-07-24 22:25:25 +0300

    spring TransactionTemplate

--- src/test/java/com/example/spring/it/database/repository/CompanyRepositoryIT.java
+++ src/test/java/com/example/spring/it/database/repository/CompanyRepositoryIT.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Repeat;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Map;
 
@@ -16,12 +18,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
-@Transactional
 class CompanyRepositoryIT extends BaseIT {
 
     EntityManager entityManager;
 
+    TransactionTemplate transactionTemplate;
+
     @Test
+    @Transactional
     void findById() {
         Company company = entityManager.find(Company.class, 1);
 
@@ -31,6 +35,20 @@ class CompanyRepositoryIT extends BaseIT {
     }
 
     @Test
+    void findByIdWithTransactionTemplate() {
+
+        transactionTemplate.executeWithoutResult(tx -> {
+            Company company = entityManager.find(Company.class, 1);
+
+            assertNotNull(company);
+            Assertions.assertThat(company.getLocales()).hasSize(2);
+        });
+
+    }
+
+    @Test
+    @Transactional
+    @Repeat(5)
     void save() {
         Company company = Company.builder()
                 .name("Apple")
