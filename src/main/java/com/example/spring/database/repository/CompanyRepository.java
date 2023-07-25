6d79a21644ad6c2bbc3daf7d10ad63e98eb3a47b
DeevS <deev@advantum.pro>
2023-07-25 09:07:43 +0300

    org.springframework.data.repository.Repositor

--- src/main/java/com/example/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/example/spring/database/repository/CompanyRepository.java
@@ -1,30 +1,14 @@
 package com.example.spring.database.repository;
 
 import com.example.spring.entity.Company;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.Repository;
 
-import java.util.Collections;
 import java.util.Optional;
 
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@RequiredArgsConstructor
-@Slf4j
-@Repository
-public class CompanyRepository implements CrudRepository<Integer, Company> {
+public interface CompanyRepository extends Repository<Company, Integer> {
 
+    Optional<Company> findById(Integer id);
 
-    @Override
-    public Optional<Company> findById(Integer id) {
-        log.info("findById method...");
-        return Optional.of(new Company(id, null, Collections.emptyMap()));
-    }
+    void delete(Company entity);
 
-    @Override
-    public void delete(Company entity) {
-
-    }
 }
