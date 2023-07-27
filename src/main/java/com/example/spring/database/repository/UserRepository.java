55bc044b06cd74cc68e1c3ae1e0dfaef23508583
DeevS <deev@advantum.pro>
2023-07-27 09:19:12 +0300

    Special parameters and Pageable

--- src/main/java/com/example/spring/database/repository/UserRepository.java
+++ src/main/java/com/example/spring/database/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.example.spring.database.repository;
 
+import com.example.spring.entity.Role;
 import com.example.spring.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -27,5 +33,21 @@ public interface UserRepository extends JpaRepository<User, Long> {
                     where u.username = :username
                     """)
     List<User> findAllByUsername(String username);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            update User u
+            set u.role = :role
+            where u.id in (:ids)
+            """)
+    int updateRole(Role role, Long... ids);
+
+    Optional<User> findFirstByOrderByIdDesc();
+
+    List<User> findFirst3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate birthDate);
+
+    List<User> findFirst3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
 
 }
--- src/test/java/com/example/spring/it/database/repository/UserRepositoryIT.java
+++ src/test/java/com/example/spring/it/database/repository/UserRepositoryIT.java
@@ -1,15 +1,23 @@
 package com.example.spring.it.database.repository;
 
 import com.example.spring.database.repository.UserRepository;
+import com.example.spring.entity.Role;
 import com.example.spring.entity.User;
 import com.example.spring.it.BaseIT;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
@@ -20,8 +28,51 @@ class UserRepositoryIT extends BaseIT {
     @Test
     void checkQueries() {
         List<User> users = userRepository.findAllByFirstnameContainingAndLastnameContaining("a", "ov");
-        Assertions.assertThat(users).hasSize(3);
+        assertThat(users).hasSize(3);
     }
 
+    @Transactional
+    @Test
+    void checkUpdate() {
+        User user = userRepository.findById(1L).orElseThrow();
+        assertSame(Role.ADMIN, user.getRole());
+
+        int result = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, result);
+
+        user = userRepository.findById(1L).orElseThrow();
+        assertSame(Role.USER, user.getRole());
+    }
+
+    @Test
+    void checkFindFirst() {
+        Optional<User> firstUser = userRepository.findFirstByOrderByIdDesc();
+        assertTrue(firstUser.isPresent());
+        firstUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
+
+    @Test
+    void checkFirst3() {
+        List<User> users = userRepository.findFirst3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate.now());
+        assertThat(users).hasSize(3);
+    }
+
+    @Test
+    void checkSort() {
+//        Sort sortById = Sort.by("firstname").and(Sort.by("lastname"));
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sort = sortBy.by(User::getFirstname)
+                .and(sortBy.by(User::getLastname));
+        List<User> users = userRepository.findFirst3ByBirthDateBefore(LocalDate.now(), sort.descending());
+        assertThat(users).hasSize(3);
+    }
+
+    @Transactional
+    @Test
+    void checkPageable(){
+        PageRequest pageable = PageRequest.of(1, 2, Sort.by("id"));
+        List<User> result = userRepository.findAllBy(pageable);
+        assertThat(result).hasSize(2);
+    }
 
 }
