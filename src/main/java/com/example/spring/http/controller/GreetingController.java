7aba8f511c471832b64ac664c7412100419c0091
DeevS <deev@advantum.pro>
2023-07-31 20:03:08 +0300

    @RequestMapping. @GetMapping

--- src/main/java/com/example/spring/http/controller/GreetingController.java
+++ src/main/java/com/example/spring/http/controller/GreetingController.java
@@ -1,19 +1,24 @@
 package com.example.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping
+@RequestMapping("/api/v1")
 public class GreetingController {
 
-    public ModelAndView hello(ModelAndView modelAndView){
+    //    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
-    public ModelAndView bye(ModelAndView modelAndView){
+    //    @RequestMapping(value = "/bye",method = RequestMethod.GET)
+    @GetMapping("/bye")
+    public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
