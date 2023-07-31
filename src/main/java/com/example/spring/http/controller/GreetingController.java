8d346dfa7261aca1b112b1667fc73ab1e1b7ad73
DeevS <deev@advantum.pro>
2023-07-31 21:22:07 +0300

    @RequestParam @RequestHeader @CookieValue @PathVariable

--- src/main/java/com/example/spring/http/controller/GreetingController.java
+++ src/main/java/com/example/spring/http/controller/GreetingController.java
@@ -1,8 +1,7 @@
 package com.example.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,8 +9,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class GreetingController {
 
     //    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView,
+                              @RequestParam("age") Integer age,
+                              @RequestParam String sex,
+                              @RequestHeader("accept") String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable Integer id) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
