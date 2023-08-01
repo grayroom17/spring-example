8b4b63a647efd29e6ae4a1e41e638c9c0d0e3741
DeevS <deev@advantum.pro>
2023-08-01 08:45:36 +0300

    Request and session scopes, @SessionAttributes

--- src/main/java/com/example/spring/http/controller/GreetingController.java
+++ src/main/java/com/example/spring/http/controller/GreetingController.java
@@ -1,11 +1,14 @@
 package com.example.spring.http.controller;
 
+import com.example.spring.dto.UserReadDto;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
     //    @RequestMapping(value = "/hello",method = RequestMethod.GET)
@@ -22,8 +25,19 @@ public class GreetingController {
 
     //    @RequestMapping(value = "/bye",method = RequestMethod.GET)
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
+        ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
+
+        return modelAndView;
+    }
+
+    @GetMapping("/hello")
+    public ModelAndView hello2(ModelAndView modelAndView,
+                               HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+
         return modelAndView;
     }
 
