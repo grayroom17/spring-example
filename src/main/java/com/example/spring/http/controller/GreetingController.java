898bfd81168dc7b6de59c8f412896783cbb42a0a
DeevS <deev@advantum.pro>
2023-08-01 09:31:45 +0300

    @ModelAttribute

--- src/main/java/com/example/spring/http/controller/GreetingController.java
+++ src/main/java/com/example/spring/http/controller/GreetingController.java
@@ -1,44 +1,36 @@
 package com.example.spring.http.controller;
 
 import com.example.spring.dto.UserReadDto;
+import com.example.spring.entity.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1")
 @SessionAttributes({"user"})
 public class GreetingController {
 
-    //    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView,
-                              @RequestParam("age") Integer age,
-                              @RequestParam String sex,
-                              @RequestHeader("accept") String accept,
-                              @CookieValue("JSESSIONID") String jsessionId,
-                              @PathVariable Integer id) {
-        modelAndView.setViewName("greeting/hello");
-        return modelAndView;
-    }
-
-    //    @RequestMapping(value = "/bye",method = RequestMethod.GET)
-    @GetMapping("/bye")
-    public ModelAndView bye(@SessionAttribute("user") UserReadDto user) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
-
-        return modelAndView;
-    }
-
     @GetMapping("/hello")
-    public ModelAndView hello2(ModelAndView modelAndView,
-                               HttpServletRequest request) {
-        modelAndView.setViewName("greeting/hello");
-        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+    public String hello2(Model model,
+                         HttpServletRequest request,
+                         @ModelAttribute UserReadDto userReadDto) {
+        model.addAttribute("user", new UserReadDto(1L, "Ivan"));
+        return "greeting/hello";
+    }
 
-        return modelAndView;
+    @GetMapping("/bye")
+    public String bye(@SessionAttribute("user") UserReadDto user, Model model) {
+        return "greeting/bye";
+    }
+
+    @ModelAttribute("roles")
+    public List<Role> roles() {
+        return Arrays.asList(Role.values());
     }
 
 }
