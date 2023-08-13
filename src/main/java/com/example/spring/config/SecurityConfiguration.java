e8412a0c885f4f78ac5e352edfe11372c2a29766
DeevS <deev@advantum.pro>
2023-08-13 21:26:32 +0300

    SecurityConfiguration,SecurityFilterChain and custom login page

---
+++ src/main/java/com/example/spring/config/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(login -> login.loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .permitAll());
+
+        return http.build();
+    }
+
+}
--- src/main/java/com/example/spring/http/controller/LoginController.java
+++ src/main/java/com/example/spring/http/controller/LoginController.java
@@ -1,11 +1,7 @@
 package com.example.spring.http.controller;
 
-import com.example.spring.dto.LoginDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -13,12 +9,6 @@ public class LoginController {
     @GetMapping("/login")
     public String loginPage() {
         return "user/login";
-    }
-
-    @PostMapping("/login")
-    public String login(Model model,
-                        @ModelAttribute("credentials") LoginDto loginDto) {
-        return "redirect:https://www.google.com";
     }
 
 }
