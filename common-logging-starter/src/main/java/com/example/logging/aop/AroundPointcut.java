2c6a4aa8f3f1bf6c5ec754960cdb68bd73337f26
DeevS <deev@advantum.pro>
2023-08-23 21:56:10 +0300

    Custom spring starter

--- common-logging-starter/src/main/java/com/example/logging/aop/AroundPointcut.java
+++ common-logging-starter/src/main/java/com/example/logging/aop/AroundPointcut.java
@@ -1,19 +1,16 @@
-package com.example.spring.aop;
+package com.example.logging.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
-@Component
-@Order(2)
 public class AroundPointcut {
 
-    @Around(value = "com.example.spring.aop.FirstAspect.anyFindByIdServiceMethod() && target(service) && args(id)", argNames = "joinPoint,service,id")
+    @Around(value = "com.example.logging.aop.FirstAspect.anyFindByIdServiceMethod() && target(service) && args(id)", argNames = "joinPoint,service,id")
     public Object addLoggingAround(ProceedingJoinPoint joinPoint, Object service, Object id) throws Throwable {
         log.info("AROUND before - invoked findById method in class {}, with id {}", service, id);
         try {
